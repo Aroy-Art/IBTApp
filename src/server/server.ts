13fb9c6648bf46cecb94fac3593c255c9911d83a
Aroy <me@aroy-art.com>
2026-09-23 11:39:59 +0200

    add logging and catch all

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import type { Request, Response } from "express";
+
 import {
 	loadTransactions,
 	saveTransactions,
@@ -13,6 +15,22 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// log the requests to terminal
+app.use((req: Request, res: Response, next) => {
+	res.on("finish", () => {
+		console.log(`${req.method} ${req.url} ${res.statusCode}`);
+	});
+
+	next();
+});
+
+// Routes Here ↓↓↓ (eg GET, PUT, POST...)
+
+// Catch all other paths and return 404
+app.use((_req: Request, res: Response) => {
+	res.status(404).json({ error: "Not Found" });
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on ${PORT}`);
