0ad2f3bbab5ef135e9cc4f4a953d2c9143912e84
Aroy <me@aroy-art.com>
2026-09-23 11:17:34 +0200

    import the data funcs and types

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express from "express";
+
+import {
+	loadTransactions,
+	saveTransactions,
+	loadClassificationRules,
+	classify,
+} from "./data";
+
+import type { Transaction } from "./data";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.listen(PORT, ()=>{
-     console.log(`Server is running on ${PORT}` )
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`);
 });
