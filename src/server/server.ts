339bde83bbd89b1b80e0d429b312fc025f613868
Fu94f <fulvia.franco1994@gmail.com>
2026-09-23 14:19:51 +0200

    fix typo transaction

--- src/server/server.ts
+++ src/server/server.ts
@@ -30,7 +30,7 @@ app.use((req: Request, res: Response, next) => {
 
 app.get('/transactions/:id', (req: Request, res:Response) => {
   const id = Number(req.params.id);
-  const transaction = loadTransactions();
+  const transactions = loadTransactions();
   const transaction = transactions.find((t) => t.id === id);
 
   if (!transaction) {
