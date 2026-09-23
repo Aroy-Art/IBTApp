4796e92a8914d2cb98562cea267ebef1cb80ea2c
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-23 13:59:09 +0200

    feat: get transactions

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,13 @@
+import type { Request, Response } from "express";
 import express from "express";
 
-import type { Request, Response } from "express";
-
-import {
-	loadTransactions,
-	saveTransactions,
-	loadClassificationRules,
-	classify,
-} from "./data";
-
 import type { Transaction } from "./data";
+import {
+  classify,
+  loadClassificationRules,
+  loadTransactions,
+  saveTransactions,
+} from "./data";
 
 const app = express();
 const PORT = 3000;
@@ -19,11 +17,11 @@ app.use(express.json());
 
 // log the requests to terminal
 app.use((req: Request, res: Response, next) => {
-	res.on("finish", () => {
-		console.log(`${req.method} ${req.url} ${res.statusCode}`);
-	});
+  res.on("finish", () => {
+    console.log(`${req.method} ${req.url} ${res.statusCode}`);
+  });
 
-	next();
+  next();
 });
 
 //get transaction by id
@@ -43,13 +41,19 @@ app.get('/transactions/:id', (req: Request, res:Response) => {
 
 
 
+// Get transactions
+app.get("/transactions", (_req: Request, res: Response) => {
+  const transactions: Transaction[] = loadTransactions();
+  res.json(transactions);
+});
+
 // Catch all other paths and return 404
 app.use((_req: Request, res: Response) => {
-	res.status(404).json({ error: "Not Found" });
+  res.status(404).json({ error: "Not Found" });
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
 
 
