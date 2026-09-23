3a31d08b9915a1e94da80ddcdc43fb0ed2f5bb26
Aroy <me@aroy-art.com>
2026-09-23 15:46:39 +0200

    add: isDateString utils func

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,5 +1,9 @@
+export function isDateString(date: string): boolean {
+	return /^\d{4}-\d{2}-\d{2}$/.test(date);
+}
+
 export function isUUID(uuid: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(
-    uuid,
-  );
+	return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(
+		uuid,
+	);
 }
