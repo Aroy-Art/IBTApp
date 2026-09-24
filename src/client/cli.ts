3a856f6088c9a9159339a3f0705abfb4e9be606d
Fu94f <fulvia.franco1994@gmail.com>
2026-09-24 15:20:35 +0200

    add transaction

--- src/client/cli.ts
+++ src/client/cli.ts
@@ -1,4 +1,4 @@
-import { confirm, input, select } from "@inquirer/prompts";
+import { confirm, input, number, select } from "@inquirer/prompts";
 
 const BASE_URL = "http://localhost:3000";
 
@@ -24,6 +24,30 @@ async function selectTransaction(): Promise<string> {
 async function viewAll() {
   const transactions = await apiFetch("/transactions");
   console.table(transactions);
+}
+
+
+
+
+
+
+
+
+
+async function addTransaction() {
+  const date = await input({ message: "Insert date (YYYY-MM-DD):"});
+  const recipient = await input({ message: "Insert recipient :"});
+  const amount = await number({ message: "Insert amount:" });
+
+  const newTransaction = await apiFetch("/transactions", {
+    method: "POST",
+    headers: {"Content-Type": "application/json" },
+    body: JSON.stringify({ date, recipient, amount })
+  });
+
+
+  console.log(newTransaction.message)
+  console.table(newTransaction.transaction);
 }
 
 // Main cli loop
@@ -52,6 +76,10 @@ async function main() {
         case "all":
           await viewAll();
           break;
+        case "add":
+          await addTransaction();
+          break;
+
       }
     } catch (err) {
       console.error(`\nError: ${(err as Error).message}\n`);
